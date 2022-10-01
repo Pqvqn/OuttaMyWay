fec058189f2077ea0a55367b491a8a4fcd08664c
Spencer Lee <spencer.hayes.lee@gmail.com>
2022-09-30 20:39:38 -0400

    Start work on civilian

---
+++ Assets/CivilianGenerator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CivilianGenerator : MonoBehaviour
+{
+    public GameObject civilianPrefab;
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject go = GameObject.Instantiate(civilianPrefab);
+            go.transform.position = new Vector3(Random.Range(-2, 2), Random.Range(-2, 2), -1);
+        }
+    }
+}
