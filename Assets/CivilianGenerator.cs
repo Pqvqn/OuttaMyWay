d7928f3078d6c3c9f6b790b8a1efb0d9917ed347
Pqvqn <pavanarauch@hotmail.com>
2022-09-30 22:25:12 -0400

    Began Player

--- Assets/CivilianGenerator.cs
+++ Assets/CivilianGenerator.cs
@@ -7,7 +7,7 @@ public class CivilianGenerator : MonoBehaviour
     public GameObject civilianPrefab;
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 100; i++)
         {
             GameObject go = GameObject.Instantiate(civilianPrefab);
             go.transform.position = new Vector3(Random.Range(-2, 2), Random.Range(-2, 2), -1);
