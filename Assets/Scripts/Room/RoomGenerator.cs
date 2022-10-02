5cea2edaec4f609311558ae023f20850b84b5aea
Spencer Lee <spencer.hayes.lee@gmail.com>
2022-10-01 22:47:20 -0400

    WIP border

--- Assets/Scripts/Room/RoomGenerator.cs
+++ Assets/Scripts/Room/RoomGenerator.cs
@@ -7,11 +7,15 @@ public class RoomGenerator : MonoBehaviour
     public GameObject civilianPrefab;
     void Start()
     {
+        int TOTAL_ROOMS = 10;
+        int TARGET_CIVILIANS = 250;
         List<SquareRoom> rooms = new List<SquareRoom>();
         SquareRoom lastRoom = null;
         int x = 0, y = 0;
         float totalArea = 0;
-        for (int i = 0; i < 10; i++)
+        int vertSize = 4 * TOTAL_ROOMS;
+        Vector2[] vertices = new Vector2[vertSize];
+        for (int i = 0; i < TOTAL_ROOMS; i++)
         {
             int width = Random.Range(6, 15), height = Random.Range(6, 12), dy = Random.Range(-height/2, height/2);
 
@@ -21,6 +25,28 @@ public class RoomGenerator : MonoBehaviour
             {
                 room.Prev = lastRoom;
                 lastRoom.Next = room;
+                if (lastRoom.top < room.top)
+                {
+                    vertices[2 * i] = new Vector2(room.left, lastRoom.top);
+                    vertices[2 * i + 1] = new Vector2(room.left, room.top);
+                    vertices[vertSize - 2 * i] = new Vector2(lastRoom.right, room.bottom);
+                    vertices[vertSize - 2 * i - 1] = new Vector2(lastRoom.right, lastRoom.bottom);
+                } else
+                {
+                    vertices[2 * i] = new Vector2(lastRoom.right, lastRoom.top);
+                    vertices[2 * i + 1] = new Vector2(lastRoom.right, room.top);
+                    vertices[vertSize - 2 * i] = new Vector2(room.left, room.bottom);
+                    vertices[vertSize - 2 * i - 1] = new Vector2(room.left, lastRoom.bottom);
+                }
+            } else
+            {
+                vertices[0] = new Vector2(room.left, room.bottom);
+                vertices[1] = new Vector2(room.left, room.top);
+            }
+            if (i == TOTAL_ROOMS - 1)
+            {
+                vertices[vertSize - 2 * i - 3] = new Vector2(room.right, room.top);
+                vertices[vertSize - 2 * i - 2] = new Vector2(room.right, room.bottom);
             }
             lastRoom = room;
             rooms.Add(room);
@@ -31,8 +57,7 @@ public class RoomGenerator : MonoBehaviour
             x += width - 4;
             y += dy;
         }
-
-        int TARGET_CIVILIANS = 250;
+        GetComponent<EdgeCollider2D>().SetPoints(new List<Vector2>(vertices));
         foreach (SquareRoom room in rooms)
         {
             int citizens = Mathf.FloorToInt(TARGET_CIVILIANS * room.area / totalArea);
