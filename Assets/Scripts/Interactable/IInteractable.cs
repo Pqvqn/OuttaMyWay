882db4291065634653fc51ff84c8fd6d37ad2dd4
Pqvqn <pavanarauch@hotmail.com>
2022-10-02 17:14:09 -0400

    Better Holding headers

--- Assets/Scripts/Interactable/IInteractable.cs
+++ Assets/Scripts/Interactable/IInteractable.cs
@@ -5,6 +5,8 @@ public interface IInteractable
     public bool KnockDown();
     public bool ApplyForce(Vector2 force);
     public IInteractable Holder { get; set; }
+    public IInteractable Holdee { get; }
+    public IInteractable Hold(IInteractable target);
     public Vector2 HoldDirection();
     public Vector2 Position();
 }
